7b5c034a5e0b251ac68f605c5c525602bb2362a6
joaquin30 <46060104+joaquin30@users.noreply.github.com>
2023-06-25 11:23:57 -0500

    confirmation on server and client console structure

--- client.py
+++ client.py
@@ -4,11 +4,30 @@ import asyncio
 import sys
 from websockets.sync.client import connect
 
-def hello():
-    with connect(f"ws://localhost:8765/?user={sys.argv[1]}") as websocket:
-        websocket.send("Hello world!")
-        return 
-        message = websocket.recv()
-        print(f"Received: {message}")
+# def hello():
+#     with connect(f"ws://localhost:8765/?user={sys.argv[1]}") as websocket:
+#         websocket.send("Hello world!")
+#         return 
+#         message = websocket.recv()
+#         print(f"Received: {message}")
 
-hello()
+
+def main():
+    while True:
+        commands = input('EpicChat> ').split()
+        if len(commands) > 2:
+            print('Command not recognized')
+        elif len(commands) == 1 and (commands[0] == 'quit' or commands[0] == 'q'):
+            break
+        elif commands[0] == 'read' or commands[0] == 'r':
+            #read()
+            print("Reading..")
+        elif commands[0] == 'write' or commands[0] == 'w':
+            msg = input(f'To {commands[1]}: ')
+            print(msg)
+            #write(msg)
+        else:
+            print('Command not recognized')
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -8,6 +8,7 @@ connections = {}
 messages_to_send = {}
 messages_sent = {}
 messages_received = {}
+confirmations = {}
 
 async def handler(websocket):
     # for obtaining username in "/?user=<username>"
@@ -26,61 +27,68 @@ async def handler(websocket):
     try:
         async for message in websocket:
             json_message = json.loads(message)
-            # for logging
-            print(json_message)
-            messages_sent[user].put((json_message['timestamp'], json_message))
-            messages_to_send[user].put((json_message['timestamp'], json_message))
+            print(json_message) # for logging
+            
+            if json_message['type'] == 'JM01': # Message
+                await messages_sent[user].put((json_message['timestamp'], json_message))
+                await messages_to_send[user].put((json_message['timestamp'], json_message['local_id']))
+            elif json_message['type'] == 'JM05': # Confirmations
+                await confirmations[json_message['confirmation_of']].put(True)
     except:
         del connections[user]
         
 
-def changeStatus(msg_id, old, new):
+def changeStatus(msg_id, sent=False, received=False):
     json_msg = {
         'type': 'JM03',
-        'message_id': msg_id,   
-        'old_status': old,
-        'new_status': new
+        'message_id': msg_id,
     }
+    
+    if received:
+        json_msg['type'] = 'JM04'
+        
     return json.dumps(json_msg)
 
-async def update():
+async def processQueues():
     while True:
         for user, websocket in connections.items():
             if not messages_to_send[user].empty():
-                timestamp, message = messages_to_send[user].get()
+                timestamp, message = await messages_to_send[user].get()
                 try:
                     msg_for_receiver = message
                     del msg_for_receiver['local_id']
                     await websocket.send(json.dumps(msg_for_receiver))
-                    # TODO confirmation
-                    messages_received.put((datetime.now(), message['local_id']))
+                    await confirmations['JM02'].get()
+                    await messages_received[user].put((datetime.now(), message['local_id']))
                 except:
-                    messages_to_send[user].put((timestamp, msg))
+                    await messages_to_send[user].put((timestamp, message))
 
             if not messages_sent[user].empty():
-                timestamp, message_id = messages_sent[user].get()
+                timestamp, message_id = await messages_sent[user].get()
                 try:
-                    await websocket.send(changeStatus(message_id, 0, 1))
-                    # TODO confirmation
+                    await websocket.send(changeStatus(message_id, sent=True))
+                    await confirmations['JM03'].get()
                 except:
-                    messages_sent[user].put((timestamp, message_id))
+                    await messages_sent[user].put((timestamp, message_id))
 
             if not messages_received[user].empty():
-                timestamp, message_id = messages_received[user].get()
+                timestamp, message_id = await messages_received[user].get()
                 try:
-                    await websocket.send(changeStatus(message_id, 1, 2))
-                    # TODO confirmation
+                    await websocket.send(changeStatus(message_id, received=True))
+                    await confirmations['JM04'].get()
                 except:
-                    messages_received[user].put((timestamp, message_id))
+                    await messages_received[user].put((timestamp, message_id))
         
-        # For low CPU usage
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(0.1) # For low CPU usage
                 
 
 async def main():
     async with serve(handler, 'localhost', 8765):
-        await asyncio.Future()  # run forever
+        await asyncio.Future() # run forever
 
 if __name__ == '__main__':
-    asyncio.run(update())
+    for i in range(2, 5):
+        confirmations[f'JM0{i}'] = asyncio.Queue()
+        
+    asyncio.run(processQueues())
     asyncio.run(main())
